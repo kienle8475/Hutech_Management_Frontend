e96c57dedbfe907a65e34b816c46841b49242c78
root <root@ip-172-31-30-193.ap-southeast-1.compute.internal>
2021-01-08 16:29:13 +0000

    first commit

---
+++ src/views/pages/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="c-app flex-row align-items-center">
+    <CContainer>
+      <CRow class="justify-content-center">
+        <CCol md="8">
+          <CCardGroup>
+            <CCard class="p-4">
+              <CCardBody>
+                <CForm>
+                  <h1>Login</h1>
+                  <p class="text-muted">Sign In to your account</p>
+                  <CInput placeholder="Username" autocomplete="username email">
+                    <template #prepend-content
+                      ><CIcon name="cil-user"
+                    /></template>
+                  </CInput>
+                  <CInput
+                    placeholder="Password"
+                    type="password"
+                    autocomplete="curent-password"
+                  >
+                    <template #prepend-content
+                      ><CIcon name="cil-lock-locked"
+                    /></template>
+                  </CInput>
+                  <CRow>
+                    <CCol col="6" class="text-left">
+                      <CButton color="primary" class="px-4">Login</CButton>
+                    </CCol>
+                  </CRow>
+                </CForm>
+              </CCardBody>
+            </CCard>
+          </CCardGroup>
+        </CCol>
+      </CRow>
+    </CContainer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
