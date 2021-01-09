195523c0f7acf20dd9914b22fce0539c45bb31d5
root <root@ip-172-31-30-193.ap-southeast-1.compute.internal>
2021-01-09 02:24:24 +0000

    Configuration firebase

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  lintOnSave: false,
+  lintOnSave: true,
   runtimeCompiler: true,
   configureWebpack: {
     //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
@@ -7,6 +7,15 @@ module.exports = {
       symlinks: false
     }
   },
+  // devServer: {
+  //   host: '0.0.0.0',
+  //   port: 80,
+  //   proxy: {
+  //     [`/${process.env.VUE_APP_API}`]: {
+  //       target: `${process.env.VUE_APP_PROXY}`
+  //     }
+  //   }
+  // },
   transpileDependencies: [
     '@coreui/utils',
     '@coreui/vue'
